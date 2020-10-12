601cdb437b24593a637745900e5e4bdafddd68f7
Carlota Pons <carlotaponsdm@gmail.com>
2020-10-12 17:38:39 +0100

    kata1 verson 3

--- kata1/src/kata1/Kata1.java
+++ kata1/src/kata1/Kata1.java
@@ -1,14 +1,15 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Month;
+
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date = GregorianCalendar.getInstance();
-        date.set(1999, 8, 24);
-        Person person = new Person("Lotta", date);
+        LocalDate date = LocalDate.of(1999, Month.MARCH, 24);
+        Person person;
+        person = new Person("Lotta", date);
         System.out.println("nombre: "+ person.getName()+ " Edad: "+person.getAge());
     }
 
